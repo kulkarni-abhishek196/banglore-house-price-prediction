d03e90be1bd0b460221cf3ae393fd234fc3efd1f
kulkarni-abhishek196 <kulkarniabhishek351@gmail.com>
2026-09-21 18:59:36 +0530

    added folder structure to the project

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import joblib
 reg, X_test, y_test = training_lr('/Users/apple/Developer/ML/bangalore-house-price-prediction/datasets/dataset.csv')
 
 os.makedirs('models', exist_ok=True)
-joblib.dump(reg, 'models/churn_model.pkl')
+joblib.dump(reg, 'models/house_prediction_model.pkl')
 print("Model saved.")
 
 evaluate_lr(reg, X_test, y_test)
